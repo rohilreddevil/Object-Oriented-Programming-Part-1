9e3733cb772e83f0709c17144bc16741bf70f596
Rohil Khakhar <38146225+rohilreddevil@users.noreply.github.com>
2020-08-26 10:51:02 -0400

    Add files via upload

---
+++ Workshops/08/ChequingAccount.h
@@ -0,0 +1,31 @@
+
+#ifndef SICT_CHEQUINGACCOUNT_H__
+#define SICT_CHEQUINGACCOUNT_H__
+
+#include "Account.h"
+
+namespace sict {
+	class ChequingAccount   :public Account  {
+
+		double transaction_fee;
+		double month_end_fee;
+
+		public:
+			// TODO: constructor initializes account balance and transaction fee
+			ChequingAccount(double, double, double);
+
+			// TODDO: credit adds +ve amount to the balance
+			bool credit(double);
+
+			// TODO: debit subtracts a +ve amount from the balance
+			bool debit(double);
+
+			// TODO: month end
+			void monthEnd();
+
+			// TODO: display inserts the account information into an ostream
+			void display(std::ostream&) const;
+
+	};
+}
+#endif
